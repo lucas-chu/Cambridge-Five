af916168aa6cc89068d01364a3c983a218888428
Lucas Chu <chu@Lucass-MacBook-Pro.local>
2020-03-19 00:28:00 -0400

    textract

--- pdfocr.py
+++ pdfocr.py
@@ -1,48 +1,111 @@
-import os, io
-import re
-from google.cloud import vision
-from google.cloud import storage
-from google.protobuf import json_format
+import boto3
+import time
 
-"""
-# pip install --upgrade google-cloud-storage
-"""
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'Cambridge Five OCR-13374180dee6.json'
-client = vision.ImageAnnotatorClient()
+def startJob(s3BucketName, objectName):
+    response = None
+    client = boto3.client('textract')
+    response = client.start_document_text_detection(
+    DocumentLocation={
+        'S3Object': {
+            'Bucket': s3BucketName,
+            'Name': objectName
+        }
+    })
 
-batch_size = 2
-mime_type = 'application/pdf'
-feature = vision.types.Feature(
-    type=vision.enums.Feature.Type.DOCUMENT_TEXT_DETECTION)
-for name in ['KV 4_466','KV 4_467','KV 4_468','KV 4_469-2','KV 4_469-3','KV 4_469','KV 4_470','KV 4_471','KV 4_472','KV 4_473','KV 4_474','KV 4_475','KV-4-185_1','KV-4-185_2','KV-4-186_1','KV-4-186_2','KV-4-187_1','KV-4-187_2','KV-4-188_1','KV-4-188_2','KV-4-189_1','KV-4-190_1','KV-4-190_2','KV-4-191_1','KV-4-191_2','KV-4-192_1','KV-4-192_2','KV-4-192_3','KV-4-193_1','KV-4-193_2','KV-4-193_3','KV-4-194_1','KV-4-194_2','KV-4-195_1','KV-4-195_2','KV-4-195_3','KV-4-196_1','KV-4-196_2','KV-4-196_3']:
-  gcs_source_uri = 'gs://cambridgefivepdfs/wetransfer-5e0e14/' + name + '.pdf'
-  gcs_source = vision.types.GcsSource(uri=gcs_source_uri)
-  input_config = vision.types.InputConfig(gcs_source=gcs_source, mime_type=mime_type)
+    return response["JobId"]
 
-  gcs_destination_uri = 'gs://cambridgefivepdfs/results/pdf_result'
-  gcs_destination = vision.types.GcsDestination(uri=gcs_destination_uri)
-  output_config = vision.types.OutputConfig(gcs_destination=gcs_destination, batch_size=batch_size)
+def isJobComplete(jobId):
+    time.sleep(5)
+    client = boto3.client('textract')
+    response = client.get_document_text_detection(JobId=jobId)
+    status = response["JobStatus"]
+    print("Job status: {}".format(status))
 
-  async_request = vision.types.AsyncAnnotateFileRequest(
-      features=[feature], input_config=input_config, output_config=output_config)
+    while(status == "IN_PROGRESS"):
+        time.sleep(5)
+        response = client.get_document_text_detection(JobId=jobId)
+        status = response["JobStatus"]
+        print("Job status: {}".format(status))
 
-  operation = client.async_batch_annotate_files(requests=[async_request])
-  operation.result(timeout=180)
+    return status
 
-  storage_client = storage.Client()
-  match = re.match(r'gs://([^/]+)/(.+)', gcs_destination_uri)
-  bucket_name = match.group(1)
-  prefix = match.group(2)
-  bucket = storage_client.get_bucket(bucket_name)
+def getJobResults(jobId):
 
-  # List object with the given prefix
-  plaintext = open(name + '.txt', 'w')
-  blob_list = list(bucket.list_blobs(prefix=prefix))
-  for blob in blob_list:
-      json_string = blob.download_as_string()
-      response = json_format.Parse(
-              json_string, vision.types.AnnotateFileResponse())
-      first_page_response = response.responses[0]
-      annotation = first_page_response.full_text_annotation
-      plaintext.write(annotation.text)
-  plaintext.close
+    pages = []
+
+    time.sleep(5)
+
+    client = boto3.client('textract')
+    response = client.get_document_text_detection(JobId=jobId)
+
+    pages.append(response)
+    print("Resultset page recieved: {}".format(len(pages)))
+    nextToken = None
+    if('NextToken' in response):
+        nextToken = response['NextToken']
+
+    while(nextToken):
+        time.sleep(5)
+
+        response = client.get_document_text_detection(JobId=jobId, NextToken=nextToken)
+
+        pages.append(response)
+        print("Resultset page recieved: {}".format(len(pages)))
+        nextToken = None
+        if('NextToken' in response):
+            nextToken = response['NextToken']
+
+    return pages
+
+# Document
+for name in [
+'KV 4_472',
+'KV 4_473',
+'KV 4_474',
+'KV 4_475',
+'KV-4-185_1',
+'KV-4-185_2',
+'KV-4-186_1',
+'KV-4-186_2',
+'KV-4-187_1',
+'KV-4-187_2',
+'KV-4-188_1',
+'KV-4-188_2',
+'KV-4-189_1',
+'KV-4-190_1',
+'KV-4-190_2',
+'KV-4-191_1',
+'KV-4-191_2',
+'KV-4-192_1',
+'KV-4-192_2',
+'KV-4-192_3',
+'KV-4-193_1',
+'KV-4-193_2',
+'KV-4-193_3',
+'KV-4-194_1',
+'KV-4-194_2',
+'KV-4-195_1',
+'KV-4-195_2',
+'KV-4-195_3',
+'KV-4-196_1',
+'KV-4-196_2',
+'KV-4-196_3',
+] :
+    s3BucketName = "textract-console-us-east-2-2d72048a-f4fb-4929-bb46-ddf477393c88"
+    documentName = name + '.pdf'
+
+    jobId = startJob(s3BucketName, documentName)
+    print(name)
+    print("Started job with id: {}".format(jobId))
+    if(isJobComplete(jobId)):
+        response = getJobResults(jobId)
+
+    #print(response)
+
+    # Print detected text
+    f = open(name + '.txt','wb')
+    for resultPage in response:
+        for item in resultPage["Blocks"]:
+            if item["BlockType"] == "LINE":
+                f.write (item["Text"])
+    f.close()
