aaf7968092f0b441d73bf598bcfe76b49ec49261
Lucas Chu <chu@Lucass-MacBook-Pro.local>
2020-04-07 12:58:51 -0400

    update

---
+++ test/pdfocrdone.py
@@ -0,0 +1,38 @@
+import boto3
+import time
+def getJobResults(jobId):
+
+    pages = []
+
+    time.sleep(5)
+
+    client = boto3.client('textract')
+    response = client.get_document_text_detection(JobId=jobId)
+
+    pages.append(response)
+    print("Resultset page recieved: {}".format(len(pages)))
+    nextToken = None
+    if('NextToken' in response):
+        nextToken = response['NextToken']
+
+    while(nextToken):
+        time.sleep(5)
+
+        response = client.get_document_text_detection(JobId=jobId, NextToken=nextToken)
+
+        pages.append(response)
+        print("Resultset page recieved: {}".format(len(pages)))
+        nextToken = None
+        if('NextToken' in response):
+            nextToken = response['NextToken']
+
+    return pages
+response = getJobResults("e5abe1cc6e280c6a60a7e0c5e77b34bcd695403c4e876eae4c61942f6d60eb4b")
+
+#print(response)
+
+# Print detected text
+for resultPage in response:
+    for item in resultPage["Blocks"]:
+        if item["BlockType"] == "LINE":
+            print ('\033[94m' +  item["Text"] + '\033[0m')
